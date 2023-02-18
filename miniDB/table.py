6222ca1cb632a3bede5bb0f48536692f9c1999fe
georgesyrigos <georgesirigosaek@gmail.com>
2023-02-18 15:30:36 +0200

    hash update

--- miniDB/table.py
+++ miniDB/table.py
@@ -362,9 +362,9 @@ class Table:
 
         # sequential
         rows1 = []
-        opsseq = 0
+        ops = 0
         for ind, x in enumerate(column):
-            opsseq += 1
+            ops += 1
             if get_op(operator, x, value):
                 rows1.append(ind)
 
@@ -397,6 +397,74 @@ class Table:
                 row for row in s_table.data if row is not None][:int(limit)]
 
         return s_table
+
+    #select where hash
+    def _select_where_with_hash(self, return_columns, hash_map, condition, distinct=False, order_by=None, desc=True, limit=None):  
+        if return_columns == '*':
+            return_cols = [i for i in range(len(self.column_names))]
+        else:
+            return_cols = [self.column_names.index(colname) for colname in return_columns]
+
+        column_name, operator, value = self._parse_condition(condition)
+
+        #if column_name != self.column_names[self.pk_idx]:  #DEN Yparxei pleon autos o periorismos
+            #print('Column is not PK. Aborting')
+
+        rows = []
+        if (operator == '<' or operator == '>'):
+            column = self.column_by_name(column_name)
+
+            # sequential
+
+            ops = 0
+            for ind, x in enumerate(column):
+                ops+=1
+                if get_op(operator, x, value):
+                    rows.append(ind)
+
+        else:
+
+            # hash value
+            h_sum = 0
+            for letter in value:
+                h_sum += ord(letter)
+
+            h_index = h_sum % int(hash_map[0][0][0])
+
+            # find in dictionary with h_index
+            for item in hash_map[h_index]:
+                if hash_map[h_index][item][0] == hash_map[0][0][0]:
+                    continue
+
+                if hash_map[h_index][item][1] == value:
+                    rows.append(hash_map[h_index][item][0])
+
+        try:
+            k = int(limit)
+        except TypeError:
+            k = None
+        # same as simple select from now on
+        rows = rows[:k]
+        # TODO: this needs to be dumbed down
+        dict = {(key):([[self.data[i][j] for j in return_cols] for i in rows] if key=="data" else value) for key,value in self.__dict__.items()}
+
+        dict['column_names'] = [self.column_names[i] for i in return_cols]
+        dict['column_types']   = [self.column_types[i] for i in return_cols]
+
+        s_table = Table(load=dict)
+
+        s_table.data = list(set(map(lambda x: tuple(x), s_table.data))) if distinct else s_table.data
+
+        if order_by:
+            s_table.order_by(order_by, desc)
+
+        if isinstance(limit,str):
+            s_table.data = [row for row in s_table.data if row is not None][:int(limit)]
+
+        return s_table
+
+
+
 
     def order_by(self, column_name, desc=True):
         '''
