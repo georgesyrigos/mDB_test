0d3fbfded9c7011fc573f618ad00d1d3c25a2e83
Iraklis Memos <iramem18@gmail.com>
2023-02-20 18:27:42 +0200

    delete import

--- miniDB/database.py
+++ miniDB/database.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from misc import split_condition
 from btree import Btree
 from joins import Inlj, Smj
-from miniDB import table
 import pickle
 from time import sleep, localtime, strftime
 import os
